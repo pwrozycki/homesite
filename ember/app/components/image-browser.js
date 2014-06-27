50944517aacfa3cb829a33fc7ff8a167b4e3c274
pwrozycki <pwrozycki@gmail.com>
2014-06-27 21:20:43 +0200

    dodanie i podpiecie pluginu lazy load

--- ember/app/components/image-browser.js
+++ ember/app/components/image-browser.js
@@ -1,27 +1,6 @@
 export default Ember.Component.extend({
-    _showVisibleImages: function() {
-        for(var i = 0; i < this.$browserNotYetLoadedImages.length; i++) {
-            var el = this.$browserNotYetLoadedImages[i];
-            /*
-            var imgRect = el.getBoundingClientRect();
-            if (imgRect.bottom >= 0 && imgRect.top <= window.innerHeight) {
-                var $this = $(el);
-                $this.attr("src", $this.attr('data-src'));
-            }*/
-        }
-    },
-
     didInsertElement: function () {
-        this.$browserNotYetLoadedImages = $("#browser").find("img");
-        this._showVisibleImages();
-        var $this = this;
-        $(window).scroll(function () {
-            $this._showVisibleImages();
-        });
-    },
-
-    willDestroyElement: function () {
-        $(window).unbind("scroll");
+        $("img.lazy").lazyload();
     },
 
     actions: {
