88f6e5189059e108f984b371ee1b94dea553b66f
pwrozycki <pwrozycki@gmail.com>
2014-07-31 14:24:26 +0200

    fixed lazy loading images

--- ember-cli/app/components/image-browser.js
+++ ember-cli/app/components/image-browser.js
@@ -1,25 +1,27 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-    lazyLoadImages: function() {
+    lazyLoadImages: function () {
         Ember.$("img.lazy").lazyload({ threshold: 200 });
     }.on('didInsertElement'),
-        
-    imagesChanged: function() {
+
+    imagesChanged: function () {
         var $this = this;
-        Ember.run.scheduleOnce('afterRender', function() {
-            $this.lazyLoadImages();
+        this.get('model.images').then(function () {
+            Ember.run.scheduleOnce('afterRender', function () {
+                $this.lazyLoadImages();
+            });
         });
-    }.observes('model.images'),
+    }.observes('model.images.@each.thubnail'),
 
     actions: {
         imageClicked: function (image) {
             this.sendAction('imageClicked', image);
         },
-        remove: function(image) {
+        remove: function (image) {
             this.sendAction('removePressed', image);
         },
-        undo: function(image) {
+        undo: function (image) {
             this.sendAction('undoPressed', image);
         }
     }
