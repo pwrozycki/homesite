94b3ef9051f5c5b49f500e78f22d74ba2845a8a6
pwrozycki <pwrozycki@gmail.com>
2014-08-17 22:26:52 +0200

    changed history mode to hash -> makes history work smoothly on nginx

--- ember-cli/app/models/directory.js
+++ ember-cli/app/models/directory.js
@@ -1,27 +1,7 @@
+import Subdirectory from "./subdirectory";
 import DS from "ember-data";
 
-export default DS.Model.extend({
-    path: DS.attr('string'),
-    thumbnail_path: DS.attr('string'),
-    preview_path: DS.attr('string'),
-
+export default Subdirectory.extend({
     images: DS.hasMany('image'),
-    parent: DS.belongsTo('directory'),
-    subdirectories: DS.hasMany('subdirectory'),
-
-    components: function () {
-        var path = this.get('path');
-        var directories = [];
-        if (path.length > 0) {
-            var elements = path.split('/');
-            for (var i = 0; i < elements.length; i++) {
-                var elementPath = elements.slice(0, elements.length - i).join('/');
-                var elementName = elements[elements.length - i - 1];
-                directories.push({ path: elementPath, name: elementName});
-            }
-        }
-        directories.push({path: '', name: 'ROOT'});
-
-        return directories.reverse();
-    }.property('path')
+    subdirectories: DS.hasMany('subdirectory')
 });
--- ember-cli/app/models/subdirectory.js
+++ ember-cli/app/models/subdirectory.js
@@ -9,5 +9,21 @@ export default DS.Model.extend({
 
     name: function () {
         return this.get('path').match(/^.*?([^/]*)$/)[1];
+    }.property('path'),
+
+    parentDirectories: function () {
+        var path = this.get('path');
+        var directories = [];
+        if (path.length > 0) {
+            var elements = path.split('/');
+            for (var i = 0; i < elements.length; i++) {
+                var elementPath = elements.slice(0, elements.length - i).join('/');
+                var elementName = elements[elements.length - i - 1];
+                directories.push({ path: elementPath, name: elementName});
+            }
+        }
+        directories.push({path: '', name: 'ROOT'});
+
+        return directories.reverse();
     }.property('path')
 });
