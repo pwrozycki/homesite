4f3129a7995e9b09d8f6fe417ad655ee61e88ba4
pwrozycki <pwrozycki@gmail.com>
2015-11-20 14:37:23 +0100

    added to preview: next, previous image scroll to image when leaving preview

--- angular/src/app/gallery/browser/browser.controller.js
+++ angular/src/app/gallery/browser/browser.controller.js
@@ -6,8 +6,27 @@
         .controller('BrowserController', BrowserController);
 
     /* @ngInject */
-    function BrowserController(directoryPromise) {
+    function BrowserController(directoryPromise, $rootScope, $window, $scope, $timeout) {
         var vm = this;
         vm.directory = directoryPromise;
+
+        $scope.$on('$stateChangeSuccess', scrollToImageOnPreviewExit);
+
+        function scrollToImageOnPreviewExit(event, toState, toParams, fromState, fromParams) {
+            var returnFromPreview = toState.name === 'main.browser' && fromState.name === 'main.browser.preview';
+            if (returnFromPreview) {
+                $timeout(function () {
+                    scrollToImage(fromParams.imageName)
+                });
+            }
+        }
+
+        function scrollToImage(imageName) {
+            var jqImage = angular.element('img[data-name="' + imageName + '"]');
+            if (jqImage.length) {
+                var scrollTop = jqImage.offset().top + jqImage.height() / 2 - $window.innerHeight / 2;
+                angular.element("body").animate({scrollTop: scrollTop}, 0);
+            }
+        }
     }
 })();
--- angular/src/app/gallery/browser/preview.controller.js
+++ angular/src/app/gallery/browser/preview.controller.js
@@ -6,10 +6,12 @@
         .controller('PreviewController', PreviewController);
 
     /* @ngInject */
-    function PreviewController(imagePromise) {
+    function PreviewController(directoryPromise, imagePromise, $state, _) {
         var vm = this;
         vm.title = 'PreviewController';
         vm.image = imagePromise;
+        vm.previousImage = previousImage;
+        vm.nextImage = nextImage;
 
         activate();
 
@@ -17,6 +19,27 @@
 
         function activate() {
 
+        }
+
+        function nextImage() {
+            switchToImage(1);
+        }
+
+        function previousImage() {
+            switchToImage(-1);
+        }
+
+        function switchToImage(sign) {
+            var currentIndex = _.findIndex(directoryPromise._images, function (el) {
+                return el._name === vm.image._name;
+            });
+
+            var newIndex = currentIndex + sign;
+
+            if (newIndex >= 0 && newIndex < directoryPromise._images.length) {
+                var newName = directoryPromise._images[newIndex]._name;
+                $state.go('^.preview', {imageName: newName})
+            }
         }
     }
 
